cedf24423d023c50cc0dd7e03f28e5e1285d6663
Angel Concha Layme <aconchal@unsa.edu.pe>
2022-02-19 16:11:55 -0500

    Interfaces gráficas (Swing - JComboBox)

---
+++ Java/swing/JComboBox/Formulario.java
@@ -0,0 +1,38 @@
+package JComboBox;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class Formulario extends JFrame implements ItemListener{
+    private JComboBox combo1;
+
+    public Formulario(){
+        setLayout(null);
+        combo1 = new JComboBox();
+        combo1.setBounds(10,10,80,20); 
+        add(combo1);
+
+        combo1.addItem("Item 1");
+        combo1.addItem("Item 2");
+        combo1.addItem("Item 3");
+        combo1.addItem("Item 4");
+        combo1.addItem("Item 5");
+
+        combo1.addItemListener(this);
+    }
+
+    public void itemStateChanged(ItemEvent e){
+        if(e.getSource() == combo1){
+            String election;
+            election = (String)combo1.getSelectedItem();
+            setTitle(election);
+        }
+    }
+
+    public static void main(String args[]){
+        Formulario formulario = new Formulario();
+        formulario.setBounds(0,0,300,150);
+        formulario.setVisible(true);
+        formulario.setResizable(false);
+        formulario.setLocationRelativeTo(null);
+    }
+}
